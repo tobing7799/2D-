65f1461f9f7617f780164d12f25d33c4ce5bedb7
tobing7799 <ssudomt@naver.com>
2021-10-21 04:45:35 +0900

    블록 코드 계획 작성

---
+++ block.py
@@ -0,0 +1,14 @@
+from pico2d import *
+
+
+class Block():
+    def __init__(self):
+        #블록들에 필요한 변수를 선언한다.
+    def draw(self):
+        #블록들의 총 개수만큼 출력해준다.
+    def set(self):
+        #블록들에게 좌표를 지정해준다.
+    def block_return(self):
+        #메인 파일에게 블록들의 좌표를 넘겨준다.(충돌체크를 위함)
+        
+
