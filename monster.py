9f7d3351649816fddcdf7e3ac96c1b1829527006
tobing7799 <ssudomt@naver.com>
2021-11-02 21:20:58 +0900

    몬스터 코드 추가

---
+++ monster.py
@@ -0,0 +1,14 @@
+from pico2d import *
+
+
+class Monster():
+    def __init__(self):
+        #몬스터 종류 별로 초기화
+    def draw(self):
+       # 그릴 몬스터 선택해서 그리기
+    def set(self):
+        #몬스터 좌표 설정하기
+    def block_return(self):
+        #캐릭터와 충돌체크할때 필요한 좌표 넘겨주기
+        
+
