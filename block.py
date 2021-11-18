dcb9dcb84675366643bff85db556cfab20197236
tobing7799 <ssudomt@naver.com>
2021-11-18 10:22:18 +0900

    블럭 추가

--- block.py
+++ block.py
@@ -2,13 +2,86 @@ from pico2d import *
 
 
 class Block():
-    def __init__(self):
-        #블록들에 필요한 변수를 선언한다.
-    def draw(self):
-        #블록들의 총 개수만큼 출력해준다.
-    def set(self):
-        #블록들에게 좌표를 지정해준다.
-    def block_return(self):
-        #메인 파일에게 블록들의 좌표를 넘겨준다.(충돌체크를 위함)
-        
+    def __init__(self, x, y):
+        self.image = load_image('block1.png')
+        self.x = x
+        self.y = y
+        self.frame=0
 
+    def draw(self):
+        self.image.clip_draw(self.frame*18, 0, 18,19, self.x, self.y, 50, 50)
+
+    def update_block(self,x):
+        self.x += x
+
+    def change_block(self):
+        self.frame = 1
+
+class Coin():
+    def __init__(self, x, y):
+        self.image = load_image('coin.png')
+        self.x = x
+        self.y = y
+        self.frame=0
+        self.frame_control = 0
+
+    def draw(self):
+        self.image.clip_draw(self.frame*23, 0, 24, 20, self.x, self.y, 40, 40)
+
+    def update_frame_block(self):
+        self.frame_control += 1
+        if self.frame_control == 15:
+            self.frame = (self.frame+1) % 4
+            self.frame_control = 0
+
+    def update_coin(self, x):
+        self.x += x
+
+
+class Mushroom1():
+    def __init__(self, x, y):
+        self.image = load_image('ms1.png')
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        self.image.clip_draw(0, 0, 39, 39, self.x, self.y, 40, 40)
+
+    def update_mushroom1(self, x):
+        self.x += x
+
+class Mushroom2():
+    def __init__(self, x, y):
+        self.image = load_image('ms2.png')
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        self.image.clip_draw(0, 0, 39, 37, self.x, self.y, 40, 40)
+
+    def update_mushroom2(self, x):
+        self.x += x
+
+class Flower():
+    def __init__(self, x, y):
+        self.image = load_image('flower1.png')
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        self.image.clip_draw(0, 0, 37, 37, self.x, self.y, 40, 40)
+
+    def update_flower(self, x):
+        self.x += x
+
+class Star():
+    def __init__(self, x, y):
+        self.image = load_image('star1.png')
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        self.image.clip_draw(0, 0, 37, 37, self.x, self.y, 40, 40)
+
+    def update_star(self, x):
+        self.x += x
