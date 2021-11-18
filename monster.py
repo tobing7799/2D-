f62db47fbaf5ab8119008b129c745c84279e2e0a
tobing7799 <ssudomt@naver.com>
2021-11-18 10:21:52 +0900

    몬스터 구현

--- monster.py
+++ monster.py
@@ -1,14 +1,90 @@
 from pico2d import *
 
 
-class Monster():
-    def __init__(self):
-        #몬스터 종류 별로 초기화
-    def draw(self):
-       # 그릴 몬스터 선택해서 그리기
-    def set(self):
-        #몬스터 좌표 설정하기
-    def block_return(self):
-        #캐릭터와 충돌체크할때 필요한 좌표 넘겨주기
-        
+class Monster1():
+    def __init__(self, x, y):
+        self.image1 = load_image('monster1-right.png')
+        self.image2 = load_image('monster1-left.png')
+        self.image3 = load_image('monster1-stop.png')
+        self.x = x
+        self.y = y
+        self.frame = 0
+        self.frame_control = 0
+        self.turn = 1
+        self.turn_count = 0
+        self.speed = 0.3
 
+    def draw(self):
+        if self.turn == 1:
+            self.image1.clip_draw(self.frame*20, 0, 20, 20, self.x, self.y, 40, 40)
+        elif self.turn == -1:
+            self.image2.clip_draw(self.frame * 20, 0, 20, 20, self.x, self.y, 40, 40)
+        elif self.turn == 2:
+            self.image3.clip_draw(0, 0, 20, 10, self.x, self.y, 40, 20)
+
+    def update_moster1(self, x):
+        self.x += x
+
+    def update_frame_moster1(self):
+        self.frame_control += 1
+        self.x += self.speed
+        if self.frame_control == 15 and self.turn != 2:
+            self.frame = (self.frame+1) % 9
+            self.frame_control = 0
+            self.turn_count += 1
+        if self.turn_count == 30 and self.turn != 2:
+            self.turn *= -1
+            self.turn_count = 0
+            self.speed *= -1
+
+    def change_monster1(self):
+        if self.turn != 2:
+            self.turn = 2
+            self.speed = 0
+            self.y -= 10
+
+
+class Monster2():
+    def __init__(self, x, y):
+        self.image1 = load_image('monster2-right-test.png')
+        self.image2 = load_image('monster2-left-test.png')
+        self.image3 = load_image('monster2-stop.png')
+        self.x = x
+        self.y = y
+        self.frame = 0
+        self.frame_control = 0
+        self.turn = 1
+        self.turn_count = 0
+        self.speed = 0.3
+        self.frame_y = 62
+
+    def draw(self):
+        if self.turn == 1:
+            self.image1.clip_draw(self.frame * 18, 0, 18, 31, self.x, self.y, 40, 60)
+        elif self.turn == -1:
+            self.image2.clip_draw(self.frame * 18, 0, 18, 31, self.x, self.y, 40, 60)
+        elif self.turn == 2:
+            self.image3.clip_draw(0, 0, 18, 18, self.x, self.y, 40, 40)
+
+    def update_moster2(self, x):
+        self.x += x
+
+    def update_frame_moster2(self):
+        self.frame_control += 1
+        self.x += self.speed
+        if self.frame_control == 15 and self.turn != 2:
+            self.frame = (self.frame+1) % 6
+            self.frame_y -= 31
+            self.frame_control = 0
+            self.turn_count += 1
+
+        if self.turn_count == 30 and self.turn != 2:
+            self.turn *= -1
+            self.turn_count = 0
+            self.speed *= -1
+
+    def change_monster2(self):
+        if self.turn != 2:
+            self.turn = 2
+            self.speed = 0
+            self.y -= 10
