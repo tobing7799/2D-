f4f8d2f7e620e13ac2e23fab773df0f4020b5074
tobing7799 <ssudomt@naver.com>
2021-11-18 10:22:26 +0900

    배경 변경

--- background.py
+++ background.py
@@ -3,13 +3,16 @@ from pico2d import *
 
 class Map():
     def __init__(self):
-        self.image = load_image('background-side.jpg')
-        self.top,self.right=600,1250
+        self.image = load_image('background-test.jpg')
+        self.top,self.right=600,3000
         self.bottom,self.left=0,0
+
     def draw(self):
-        self.image.clip_draw(self.left,self.bottom, 2500, self.top, self.right,300)
+        self.image.clip_draw(self.left,self.bottom, 6000, self.top, self.right,300)
+
     def map_xy(self):
         return self.left,self.bottom,self.right,self.top
+
     def update_map(self,left,bottom,right,top):
         self.left=left
         self.right=right
