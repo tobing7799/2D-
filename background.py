ae15e025dc0585ff784bc46d81e965aacef7a690
tobing7799 <ssudomt@naver.com>
2021-12-09 12:01:05 +0900

    3가지 맵 추가

--- background.py
+++ background.py
@@ -3,12 +3,28 @@ from pico2d import *
 
 class Map():
     def __init__(self):
-        self.image = load_image('background-test.jpg')
+        self.image1 = load_image('background-test.jpg')
+        self.image2 = load_image('background-test0.jpg')
+        self.image3 = load_image('background-test1.jpg')
+        self.image4 = load_image('background-test2.jpg')
         self.top,self.right=600,3000
         self.bottom,self.left=0,0
+        self.stage_count=0
+        self.bgm = load_music('bgm.mp3')
+        self.bgm.set_volume(50)
+        self.bgm.repeat_play()
+        self.bgm_state=1
 
     def draw(self):
-        self.image.clip_draw(self.left,self.bottom, 6000, self.top, self.right,300)
+        if self.stage_count== 1:
+            self.image1.clip_draw(self.left,self.bottom, 6000, self.top, self.right,300)
+        if self.stage_count== 2:
+            self.image2.clip_draw(self.left,self.bottom, 6000, self.top, self.right,300)
+        if self.stage_count== 3:
+            self.image3.clip_draw(self.left,self.bottom, 6000, self.top, self.right,300)
+        if self.stage_count== 4:
+            self.image4.clip_draw(self.left,self.bottom, 6000, self.top, self.right,300)
+        
 
     def map_xy(self):
         return self.left,self.bottom,self.right,self.top
@@ -18,3 +34,6 @@ class Map():
         self.right=right
         self.top=top
         self.bottom=bottom
+
+    def update_stage_count(self,x):
+        self.stage_count = x
