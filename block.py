eb1747a33dbd7b1e2336114ccf01a1fda56f3b22
tobing7799 <ssudomt@naver.com>
2021-12-09 12:00:15 +0900

    충돌 처리 추가

--- block.py
+++ block.py
@@ -10,7 +10,6 @@ class Block():
 
     def draw(self):
         self.image.clip_draw(self.frame*18, 0, 18,19, self.x, self.y, 50, 50)
-        draw_rectangle(*self.get_bb())
 
     def update_block(self,x):
         self.x += x
@@ -29,10 +28,11 @@ class Coin():
         self.y = y
         self.frame=0
         self.frame_control = 0
+        self.coin_count=0
+        self.image_life = load_image('life_image.png')
 
     def draw(self):
         self.image.clip_draw(self.frame*23, 0, 24, 20, self.x, self.y, 40, 40)
-        draw_rectangle(*self.get_bb())
 
     def update_frame_block(self):
         self.frame_control += 1
@@ -51,64 +51,88 @@ class Mushroom1():
         self.image = load_image('ms1.png')
         self.x = x
         self.y = y
+        self.turn=0
+        self.up = 1
 
     def draw(self):
-        self.image.clip_draw(0, 0, 39, 39, self.x, self.y, 40, 40)
-        draw_rectangle(*self.get_bb())
+        if self.turn == 1 and self.up<40:
+            self.up += 1
+        self.image.clip_draw(0, 0, 39, 39, self.x, self.y+self.up, 40, 40)
 
     def update_mushroom1(self, x):
         self.x += x
 
     def get_bb(self):
-        return self.x-20,self.y-20,self.x+20,self.y+20
+        return self.x-20,self.y-20,self.x+20,self.y+20+self.up
+
+    def mushroom1_turn(self):
+        self.turn = 1
 
 class Mushroom2():
     def __init__(self, x, y):
         self.image = load_image('ms2.png')
         self.x = x
         self.y = y
+        self.turn=0
+        self.up = 1
 
     def draw(self):
-        self.image.clip_draw(0, 0, 39, 37, self.x, self.y, 40, 40)
-        draw_rectangle(*self.get_bb())
+        if self.turn == 1 and self.up<40:
+            self.up += 1
+        self.image.clip_draw(0, 0, 39, 37, self.x, self.y+self.up, 40, 40)
 
     def update_mushroom2(self, x):
         self.x += x
 
     def get_bb(self):
-        return self.x-20,self.y-20,self.x+20,self.y+20
+        return self.x-20,self.y-20,self.x+20,self.y+20+self.up
+    
+    def mushroom2_turn(self):
+        self.turn = 1
 
 class Flower():
     def __init__(self, x, y):
         self.image = load_image('flower1.png')
         self.x = x
         self.y = y
+        self.turn=0
+        self.up = 1
 
     def draw(self):
-        self.image.clip_draw(0, 0, 37, 37, self.x, self.y, 40, 40)
-        draw_rectangle(*self.get_bb())
+        if self.turn == 1 and self.up<40:
+            self.up += 1
+        self.image.clip_draw(0, 0, 37, 37, self.x, self.y+self.up, 40, 40)
 
     def update_flower(self, x):
         self.x += x
 
     def get_bb(self):
-        return self.x-20,self.y-20,self.x+20,self.y+20
+        return self.x-20,self.y-20,self.x+20,self.y+20+self.up
+
+    def flower_turn(self):
+        self.turn = 1
     
 class Star():
     def __init__(self, x, y):
         self.image = load_image('star1.png')
         self.x = x
         self.y = y
+        self.turn=0
+        self.up = 1
 
     def draw(self):
-        self.image.clip_draw(0, 0, 37, 37, self.x, self.y, 40, 40)
-        draw_rectangle(*self.get_bb())
+        if self.turn == 1 and self.up<40:
+             self.up += 1
+        self.image.clip_draw(0, 0, 37, 37, self.x, self.y+self.up, 40, 40)
 
     def update_star(self, x):
         self.x += x
 
     def get_bb(self):
-        return self.x-20,self.y-20,self.x+20,self.y+20
+        return self.x-20,self.y-20,self.x+20,self.y+20+self.up
+    
+    def star_turn(self):
+        self.turn = 1
     
 class Flag():
     def __init__(self, x, y):
@@ -118,7 +142,6 @@ class Flag():
 
     def draw(self):
         self.image.clip_draw(0, 0, 236, 854, self.x, self.y, 100, 360)
-        draw_rectangle(*self.get_bb())
 
     def update_flag(self, x):
         self.x += x
